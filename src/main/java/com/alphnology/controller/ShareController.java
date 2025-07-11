2d17aed4e27b369b0f529018c03f5b724cf78d5a
fredpena <f.ant.pena@gmail.com>
2025-07-11 17:49:52 -0400

    feat: Add event schedule URL to configuration and update ShareController to use it for sharing session details

--- src/main/java/com/alphnology/controller/ShareController.java
+++ src/main/java/com/alphnology/controller/ShareController.java
@@ -1,41 +1,50 @@
 package com.alphnology.controller;
 
 import com.alphnology.data.Event;
+import com.alphnology.services.EventService;
 import com.alphnology.services.SessionService;
-import com.alphnology.utils.CommonUtils;
-import com.vaadin.flow.server.VaadinSession;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 /**
  * @author me@fredpena.dev
  * @created 11/07/2025  - 16:55
  */
 @Controller
-@RequiredArgsConstructor
 public class ShareController {
 
+    private final EventService eventService;
     private final SessionService sessionService;
+    private final String baseUrl;
+
+    public ShareController(EventService eventService, SessionService sessionService, @Value("${event.schedule}") String schedule) {
+        this.eventService = eventService;
+        this.sessionService = sessionService;
+        this.baseUrl = schedule;
+    }
 
     @GetMapping("/share/{sessionId}")
     public String sharePage(@PathVariable Long sessionId, Model model) {
-        System.out.println("sessionId: " + sessionId);
         sessionService.get(sessionId)
                 .ifPresent(session -> {
 
-                    Event event = VaadinSession.getCurrent().getAttribute(Event.class);
+                    Optional<Event> optionalEvent = eventService.findAll().stream().findFirst();
+                    if (optionalEvent.isEmpty()) {
+                        return;
+                    }
 
-                    String baseUrl = CommonUtils.getBaseUrl();
                     String message = """
                             Looking forward to the "%s" session at #%s%s. 
                             
                             See you in the %s room!
                             
                             Check out the full schedule: %s
-                            """.formatted(session.getTitle(), event.getName().replace(" ", ""), session.getStartTime().getYear(), session.getRoom().getName(), baseUrl);
+                            """.formatted(session.getTitle(), optionalEvent.get().getName().replace(" ", ""), session.getStartTime().getYear(), session.getRoom().getName(), baseUrl);
 
                     String url = "%s/share/%s".formatted(baseUrl, session.getCode());
                     model.addAttribute("title", "Attending: " + session.getTitle());
